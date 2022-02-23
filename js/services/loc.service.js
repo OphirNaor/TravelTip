8e6a1012f953d8cbb6258735798a3ce60bd33425
Kyrill Belenkov <bel.kir89@gmail.com>
2022-02-23 15:38:49 +0200

    comments on TODO

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -2,10 +2,13 @@ export const locService = {
     getLocs
 }
 
+const KEY = 'location'
+// let gId = []
+
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+    { id: '1' , name: 'Greatplace', lat: 32.047104, lng: 34.832384 , createdAt: 000, updatedAt: '' }, 
+    { id: '2' , name: 'Neveragain', lat: 32.047201, lng: 34.832581 , createdAt: 000, updatedAt: '' },
 ]
 
 function getLocs() {
@@ -16,4 +19,12 @@ function getLocs() {
     });
 }
 
+function createLoc()
+
+
+
+function deleteLoc()
+//get firstly the index of the ID of loc
+//we should use the splice method , to splice the .locs
+//render the map again 
 
