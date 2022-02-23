6cb2e9fb11298245f41f914440c439bcb15f2bd9
OphirNaor <ophir1991@gmail.com>
2022-02-23 14:57:36 +0200

    API

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyCaF6bpps6lS2yH0F59Kp5SeLynY5UmUjc';
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
